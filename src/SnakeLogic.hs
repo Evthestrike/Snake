079a273bb85de3f094ed7060467475208c4e4976
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-08 00:54:58 -0700

    Implemented checking for legal snake

--- src/SnakeLogic.hs
+++ src/SnakeLogic.hs
@@ -1,13 +1,17 @@
 module SnakeLogic () where
 
-import Data.List
+import qualified Constants
+import Data.List (nub)
 import DataTypes
 
 inBounds :: Int -> Int -> Snake -> Bool
-inBounds width height (Snake _ ((x, y) : _)) = xInBounds && yInBounds
+inBounds width height (Snake _ coords) = xInBounds && yInBounds
   where
+    (x, y) = head coords
     xInBounds = x >= 0 && x < width
     yInBounds = y >= 0 && y < height
 
 noCollisions :: Snake -> Bool
 noCollisions (Snake _ coords) = nub coords == coords
+
+snakeIsLegal = (&&) <$> inBounds Constants.width Constants.height <*> noCollisions
