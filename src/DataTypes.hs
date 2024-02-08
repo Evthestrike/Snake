c94772d00989382fb207445e0685f2bdefc870b0
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-08 00:33:41 -0700

    Added Core Data Types and functions to operate on them

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -1,1 +1,21 @@
-module DataTypes () where 
+module DataTypes
+  ( Coords,
+    Direction (North, South, East, West),
+    Snake (Snake),
+    moveCoords,
+  )
+where
+
+import Data.Bifunctor
+
+type Coords = (Int, Int)
+
+data Direction = North | South | East | West deriving (Show, Read)
+
+data Snake = Snake Direction Coords deriving (Show, Read)
+
+moveCoords :: Direction -> Coords -> Coords
+moveCoords North = second (+ (-1))
+moveCoords South = second (+ 1)
+moveCoords East = first (+ 1)
+moveCoords West = first (+ (-1))
