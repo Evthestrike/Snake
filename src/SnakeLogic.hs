5cc94ee191d31f4f04d376c54a78d9d33df4b3ea
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-08 00:48:43 -0700

    Removed NonEmpty and added noCollisions

--- src/SnakeLogic.hs
+++ src/SnakeLogic.hs
@@ -1,3 +1,13 @@
 module SnakeLogic () where
 
+import Data.List
 import DataTypes
+
+inBounds :: Int -> Int -> Snake -> Bool
+inBounds width height (Snake _ ((x, y) : _)) = xInBounds && yInBounds
+  where
+    xInBounds = x >= 0 && x < width
+    yInBounds = y >= 0 && y < height
+
+noCollisions :: Snake -> Bool
+noCollisions (Snake _ coords) = nub coords == coords
