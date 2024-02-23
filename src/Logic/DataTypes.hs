91bda8fbb7b2cce7be86339955e0147e1b4ff499
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-22 22:26:13 -0700

    renamed AppState to GameState

--- src/Logic/DataTypes.hs
+++ src/Logic/DataTypes.hs
@@ -2,7 +2,7 @@ module Logic.DataTypes
   ( Coord,
     Direction (North, South, East, West),
     Snake (Snake, direction, coords),
-    AppState (AppState, randGen, appleCoord, snake),
+    GameState (GameState, randGen, appleCoord, snake),
     moveCoord,
   )
 where
@@ -16,7 +16,7 @@ data Direction = North | South | East | West deriving (Show, Read, Eq)
 
 data Snake = Snake {direction :: Direction, coords :: [Coord]} deriving (Show, Read)
 
-data AppState = AppState {randGen :: StdGen, appleCoord :: Coord, snake :: Snake}
+data GameState = GameState {randGen :: StdGen, appleCoord :: Coord, snake :: Snake}
 
 moveCoord :: Direction -> Coord -> Coord
 moveCoord North = second (+ (-1))
