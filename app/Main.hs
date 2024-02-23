f19d40f384feefa4b00172af19b69988b46ec1e7
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-22 22:24:58 -0700

    Moved app logic to App.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,72 +1,11 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RecordWildCards #-}
-
 module Main (main) where
 
+import App
 import Brick
 import Brick.BChan (newBChan, writeBChan)
-import Brick.Widgets.Border (borderWithLabel)
-import Brick.Widgets.Center (center)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad
-import qualified Graphics.Assets as Assets
-import Graphics.RenderGrid
-import Graphics.Vty
-import qualified Logic.Constants as Constants
-import Logic.DataTypes
-import Logic.SnakeLogic
 import System.Random
-
-data Tick = Tick
-
-app :: App AppState Tick ()
-app =
-  App
-    ((: []) . center . borderWithLabel (str "Snake") . grid Constants.width Constants.height)
-    (\_ _ -> Nothing)
-    handleEvent
-    (return ())
-    ( const . attrMap defAttr $
-        [ (Assets.headAttr, fg brightGreen),
-          (Assets.bodyAttr, fg green),
-          (Assets.appleAttr, fg red),
-          (Assets.groundAttr, fg white)
-        ]
-    )
-
-handleEvent :: BrickEvent n Tick -> EventM n AppState ()
-handleEvent e = do
-  (AppState {snake, ..}) <- get
-  let newSnake = case e of
-        AppEvent Tick ->
-          if (head . coords . growSnake $ snake) == appleCoord
-            then growSnake snake
-            else moveSnake snake
-        VtyEvent (EvKey KUp _) -> turnHead North snake
-        VtyEvent (EvKey KDown _) -> turnHead South snake
-        VtyEvent (EvKey KRight _) -> turnHead East snake
-        VtyEvent (EvKey KLeft _) -> turnHead West snake
-        _ -> snake
-      (newGen, newAppleCoord) =
-        if (head . coords $ newSnake) == appleCoord
-          then
-            let (newAppleX, newGen) = uniformR (0, Constants.width - 1) randGen
-                (newAppleY, newGen') = uniformR (0, Constants.height - 1) newGen
-             in (newGen', (newAppleX, newAppleY))
-          else (randGen, appleCoord)
-      newAppState =
-        AppState
-          { randGen = newGen,
-            appleCoord = newAppleCoord,
-            snake = newSnake
-          }
-
-  case e of
-    VtyEvent (EvKey KEsc _) -> halt
-    _ ->
-      if snakeIsLegal newSnake
-        then put newAppState
-        else halt
 
 main :: IO ()
 main = do
@@ -75,5 +14,5 @@ main = do
     writeBChan eventChan Tick
     threadDelay 100000
   initGen <- initStdGen
-  _ <- customMainWithDefaultVty (Just eventChan) app (AppState {randGen = initGen, appleCoord = (1, 1), snake = Snake East [(0, 0)]})
+  _ <- customMainWithDefaultVty (Just eventChan) app . initialState $ initGen
   return ()
