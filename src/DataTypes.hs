a43d75e2e692b178eb86b1577246bc2322ec242f
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 20:56:47 -0700

    Adding functions to change states

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module DataTypes
   ( Tick (Tick),
     AppMachine (Game, Menu),
   )
 where
 
+import qualified Game.Logic.Constants
 import Game.Logic.DataTypes
 import Menu.Logic.DataTypes
 
 data Tick = Tick
 
 data AppMachine = Game GameState | Menu MenuState
+
+newGame :: AppMachine -> AppMachine
+newGame (Game (GameState {randGen})) = Game . Game.Logic.Constants.defaultGameState $ randGen
+newGame (Menu (MenuState {randGen})) = Game . Game.Logic.Constants.defaultGameState $ randGen
