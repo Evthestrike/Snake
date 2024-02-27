379080c02805555620af255e0416f02bd1b37c54
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 23:58:29 -0700

    Added default menustate

--- src/Menu/Logic/Constants.hs
+++ src/Menu/Logic/Constants.hs
@@ -1,1 +1,9 @@
-module Menu.Logic.Constants () where
+module Menu.Logic.Constants
+  ( defaultMenu,
+  )
+where
+
+import Menu.Logic.DataTypes
+
+defaultMenu :: MenuState
+defaultMenu = MenuState {selected = Play}
