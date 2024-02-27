fe2fd9ad8f94de749e18f11306de8a47cf8f2fc3
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 20:37:12 -0700

    Moved data types here from Game data types

--- src/Menu/Logic/DataTypes.hs
+++ src/Menu/Logic/DataTypes.hs
@@ -1,1 +1,9 @@
-module Menu.Logic.DataTypes () where
+module Menu.Logic.DataTypes
+  ( MenuOptions (Play, Quit),
+    MenuState (MenuState, menuOptions, selected),
+  )
+where
+
+data MenuOptions = Play | Quit
+
+data MenuState = MenuState {menuOptions :: [MenuOptions], selected :: Int}
