7c01fe2ccce502034f1629f0a73d05a56c7f67ee
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 20:48:18 -0700

    Added default game State

--- src/Game/Logic/Constants.hs
+++ src/Game/Logic/Constants.hs
@@ -1,11 +1,18 @@
 module Game.Logic.Constants
   ( width,
     height,
+    defaultGameState,
   )
 where
+
+import Game.Logic.DataTypes
+import System.Random (StdGen)
 
 width :: Int
 width = 12
 
 height :: Int
 height = 12
+
+defaultGameState :: StdGen -> GameState
+defaultGameState initRandGen = GameState {randGen = initRandGen, appleCoord = (1, 1), snake = Snake East [(0, 0)]}
