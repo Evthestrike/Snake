67115104e64bb78ac6c8a242b1b332ccf32b5a0c
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 20:36:53 -0700

    Moved Data types to better files

--- src/Game/Logic/DataTypes.hs
+++ src/Game/Logic/DataTypes.hs
@@ -3,10 +3,6 @@ module Game.Logic.DataTypes
     Direction (North, South, East, West),
     Snake (Snake, direction, coords),
     GameState (GameState, randGen, appleCoord, snake),
-    MenuOptions (Play, Quit),
-    MenuState (MenuState, menuOptions, selected),
-    AppMachine (Game, Menu),
-    Tick (Tick),
     moveCoord,
   )
 where
@@ -26,12 +22,4 @@ moveCoord West = first (+ (-1))
 
 data Snake = Snake {direction :: Direction, coords :: [Coord]} deriving (Show, Read)
 
-data Tick = Tick
-
 data GameState = GameState {randGen :: StdGen, appleCoord :: Coord, snake :: Snake}
-
-data MenuOptions = Play | Quit
-
-data MenuState = MenuState {menuOptions :: [MenuOptions], selected :: Int}
-
-data AppMachine = Game GameState | Menu MenuState
