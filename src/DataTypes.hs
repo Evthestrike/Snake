16cc0291bf5f7b47dbe88dca50b567a8e6a6bf62
Evan Lauer <evanlauer@u.boisestate.edu>
2024-02-26 23:58:15 -0700

    Partially implemented function to quit game

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -8,6 +8,7 @@ where
 
 import qualified Game.Logic.Constants
 import Game.Logic.DataTypes
+import qualified Menu.Logic.Constants
 import Menu.Logic.DataTypes
 import System.Random
 
@@ -18,3 +19,6 @@ data AppMachine = Game StdGen GameState | Menu StdGen MenuState
 newGame :: AppMachine -> AppMachine
 newGame (Game randGen _) = Game randGen Game.Logic.Constants.defaultGameState
 newGame (Menu randGen _) = Game randGen Game.Logic.Constants.defaultGameState
+
+quitGame :: AppMachine -> AppMachine
+quitGame (Game randGen _) = Menu randGen Menu.Logic.Constants.defaultMenu
